347a2fe58158e72f244e241e558257cc53b4f586
alexandrmotologa <al.motologa@gmail.com>
2020-07-30 01:53:08 +0300

    o incercare

--- hw_arithm_functions.py
+++ hw_arithm_functions.py
@@ -1,4 +1,22 @@
 # def add( a, b ):
 #     result = a + b
 #     print(result)
-# add(12, 1)
+# add()
+
+
+def add(a=None, b=None):
+        while True:
+            try:
+                a = float(input("Introduceti prima cifra intreaga sau zecimala : "))
+                b = float(input("Introduceti a doua cifra intreaga sau zecimala : "))
+                if a != int(a):
+                    result = a + b
+                    print(result)
+                    break
+                else:
+                    result = int(a) + int(b)
+                    print(result)
+                    break
+            except:
+                print("That's not a valid option!")
+add()
